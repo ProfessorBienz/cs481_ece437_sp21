57edd9ecd4c4b3c307455ad8529ed1a258ea6b75
Amanda Bienz <bienz@Amandas-Work-MacBook-Pro.local>
2021-02-17 10:07:46 -0700

    Added a second test case

--- hw1/jobs.c
+++ hw1/jobs.c
@@ -23,7 +23,7 @@ int jobcmp(const void* a, const void* b)
     else return 0;
 }
 
-void job0(int time)
+void add(int time)
 {
     for (int t = 0; t < time; t++)
     {
@@ -31,15 +31,15 @@ void job0(int time)
     }
 }
 
-void job1(int time)
+void mult(int time)
 {
     for (int t = 0; t < time; t++)
     {
-        counter *= 2;
+        counter *= 3;
     }
 }
 
-void job2(int time)
+void reset(int time)
 {
     for (int t = 0; t < time; t++)
     {
@@ -47,26 +47,56 @@ void job2(int time)
     }
 }
 
-void create_jobs(int* n_jobs, Job** jobs)
+void divide(int time)
+{
+    for (int t = 0; t < time; t++)
+    {
+        counter /= 2;
+    }
+}
+
+void subtract(int time)
+{
+    for (int t = 0; t < time; t++)
+    {
+        counter -= 2;
+    }
+}
+
+
+void test0(int* n_jobs, Job** jobs)
 {
     *n_jobs = 3;
 
     Job* example_jobs = (Job*)malloc((*n_jobs)*sizeof(Job));
-    example_jobs[0].priority = 1;
-    example_jobs[0].idx = 0;
-    example_jobs[0].time = 2;
-    example_jobs[0].run_job = job0;
+    int priority[3] = {1, 1, 0};
+    int time[3] = {2, 1, 3};
+    Operation ops[3] = {add, mult, reset};
+    for (int i = 0; i < *n_jobs; i++)
+    {
+        example_jobs[i].idx = i;
+        example_jobs[i].priority = priority[i];
+        example_jobs[i].time = time[i];
+        example_jobs[i].run_job = ops[i];
+    }
+    *jobs = example_jobs;
+}
 
-    example_jobs[1].priority = 1;
-    example_jobs[1].idx = 1;
-    example_jobs[1].time = 1;
-    example_jobs[1].run_job = job1;
+void test1(int* n_jobs, Job** jobs)
+{
+    *n_jobs = 5;
 
-    example_jobs[2].priority = 0;
-    example_jobs[2].idx = 2;
-    example_jobs[2].time = 3;
-    example_jobs[2].run_job = job2;
-
+    Job* example_jobs = (Job*)malloc((*n_jobs)*sizeof(Job));
+    int priority[5] = {0,1,1,1,0};
+    int time[5] = {3,2,4,3,1};
+    Operation ops[5] = {add, mult, reset, subtract, divide};
+    for (int i = 0; i < *n_jobs; i++)
+    {
+        example_jobs[i].priority = priority[i];
+        example_jobs[i].idx = i;
+        example_jobs[i].time = time[i];
+        example_jobs[i].run_job = ops[i];
+    }
     *jobs = example_jobs;
 }
 
@@ -74,17 +104,32 @@ int main(int argc, char* argv[])
 {
     int n_jobs;
     Job* jobs;
-    create_jobs(&n_jobs, &jobs);
+    int time_slice;
+
+    test0(&n_jobs, &jobs);
     counter = 0;
     priority(n_jobs, jobs);
-    assert(counter == 14);
+    assert(counter == 21);
     free(jobs);
 
-    create_jobs(&n_jobs, &jobs);
+    test0(&n_jobs, &jobs);
     counter = 0;
-    int time_slice = 1;
+    time_slice = 1;
     priority_rr(n_jobs, jobs, time_slice);
-    assert(counter == 11);
+    assert(counter == 15);
+    free(jobs);
+
+    test1(&n_jobs, &jobs);
+    counter = 0;
+    priority(n_jobs, jobs);
+    assert(counter == -5);
+    free(jobs);
+
+    test1(&n_jobs, &jobs);
+    counter = 0;
+    time_slice = 2;
+    priority_rr(n_jobs, jobs, time_slice);
+    assert(counter == -1);
     free(jobs);
 }
 
